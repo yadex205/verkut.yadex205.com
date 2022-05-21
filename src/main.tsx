597318b333f44258094d4f4dbb8539f22380a4c2
Kanon Kakuno <yadex205@outlook.jp>
2022-05-21 16:21:48 +0900

    :fire: Remove unused files

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,10 @@ import { Route, Routes } from 'react-router-dom';
 
 import { ClientApp } from '~pages/_app';
 import IndexPage from '~pages/index';
-import AboutPage from '~pages/about';
-import VerkutPage from '~pages/verkut';
 
 const Routing: React.FC = () => (
   <Routes>
     <Route path="/" element={<IndexPage />} />
-    <Route path="/about/" element={<AboutPage />} />
-    <Route path="/verkut/" element={<VerkutPage />} />
   </Routes>
 );
 
